bfc0f4473d7bc50ddae94bf9c7059fc03ff8b20d
Chris Zuber <shgysk8zer0@gmail.com>
2017-09-29 19:11:51 -0700

    Add more quote from `fortune`

--- js/consts.js
+++ js/consts.js
@@ -10,6 +10,62 @@ export const quotes = [
 	}, {
 		text: `Unfortunately, most programmers like to play with new toys.  I have many friends who, immediately upon buying a snakebite kit, would be tempted to throw the first person they see to the ground, tie the tourniquet on him, slash him with the knife, and apply suction to the wound.`,
 		by: 'Jon Bentley'
+	}, {
+		text: 'Will your long-winded speeches never end? What ails you that you keep on arguing?',
+		by: 'Job 16:3'
+	}, {
+		text: 'Sattinger\'s Law:	It works better if you plug it in.'
+	}, {
+		text: 'Man has made his bedlam; let him lie in it.',
+		by: 'Fred Allen'
+	}, {
+		text: `	A little dog goes into a saloon in the Wild West, and beckons to
+the bartender.  "Hey, bartender, gimme a whiskey."
+	The bartender ignores him.
+	"Hey bartender, gimme a whiskey!"
+	Still ignored.
+	"HEY BARMAN!!  GIMME A WHISKEY!!"
+	The bartender takes out his six-shooter and shoots the dog in the
+leg, and the dog runs out the saloon, howling in pain.
+	Three years later, the wee dog appears again, wearing boots,
+jeans, chaps, a Stetson, gun belt, and guns.  He ambles slowly into the
+saloon, goes up to the bar, leans over it, and says to the bartender,
+"I'm here t'git the man that shot muh paw."
+`
+	}, {
+		text: `"Arthur's consciousness approached his body as from a great
+distance, and reluctantly. It had had some bad times in
+there. Slowly, nervously, it entered and settled down into
+its accustomed position.
+Arthur sat up.
+'Where am I?' he said.
+'Lord's Cricket Ground,' said Ford.
+'Fine,' said Arthur, and his consciousness stepped out
+again for a quick breather. His body flopped back on the
+grass."
+
+- Arthur coping with his return to Earth as best as he
+could.
+`
+	}, {
+		text: `Have no friends not equal to yourself.`,
+		by: 'Confucius'
+	}, {
+		text: `The majority of the stupid is invincible and guaranteed for all time.  The
+terror of their tyranny, however, is alleviated by their lack of consistency.`,
+		by: 'Albert Einstein'
+	}, {
+		text: `"Who cares if it doesn't do anything?  It was made with our new
+Triple-Iso-Bifurcated-Krypton-Gate-MOS process ..."`
+	}, {
+		text: `Hi! I'm a .signature virus! Copy me into your ~/.signature to help me spread!`
+	}, {
+		text: `The fountain code has been tightened slightly so you can no longer dip
+objects into a fountain or drink from one while you are floating in mid-air
+due to levitation.
+	Teleporting to hell via a teleportation trap will no longer occur
+if the character does not have fire resistance.`,
+		by: 'README file from the NetHack game'
 	}
 ];
 
