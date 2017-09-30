9eadf4f791cbb38fdc077abddf1315f8c413ad43
Chris Zuber <shgysk8zer0@gmail.com>
2017-09-29 18:01:08 -0700

    Add basic styles & functionality

---
+++ js/consts.js
@@ -0,0 +1,10 @@
+export const quotes = [
+	{
+		text: 'Lorem ipsum',
+		by: 'Unknown'
+	}, {
+		text: 'Foo bar',
+		by: 'John Smith'
+	},
+];
+
---
+++ js/index.js
@@ -0,0 +1,32 @@
+import {$} from './std-js/functions.js';
+import * as consts from './consts.js';
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function getQuote() {
+	const template = document.getElementById('quote-template').content.cloneNode(true);
+	const target = document.getElementById('quote-target');
+	const quote = consts.quotes[getRandomInt(0, consts.quotes.length)];
+
+	$(target).empty();
+
+	$('[data-prop="text"]', template).text = quote.text;
+	$('[data-prop="by"]', template).text = quote.by;
+	$('[data-action="random-quote"]', template).click(getQuote);
+	$('[data-action="tweet"]', template).each(tweetBtn => {
+		const url = new URL(tweetBtn.href);
+		url.searchParams.set('url', location.href);
+		url.searchParams.set('text', `Random quote: > ${quote.text} - ${quote.by}`);
+		tweetBtn.href = url.toString();
+	});
+	target.append(template);
+}
+
+$(window).ready(() => {
+	$('.cursor-wait').removeClass('cursor-wait');
+	getQuote();
+}, {once: true});
