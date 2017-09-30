fb43ec0f6b0fb4cdbaa23a5fcbd122aee9eb41c4
Chris Zuber <shgysk8zer0@gmail.com>
2017-09-29 18:14:18 -0700

    Add some quotes from `fortune`

--- js/consts.js
+++ js/consts.js
@@ -1,10 +1,18 @@
+/* eslint quotes: "off" */
+
 export const quotes = [
 	{
-		text: 'Lorem ipsum',
+		text: `Bing's Rule: Don't try to stem the tide -- move the beach.`,
 		by: 'Unknown'
 	}, {
-		text: 'Foo bar',
-		by: 'John Smith'
-	},
+		text: 'Never call a man a fool.  Borrow from him.',
+		by: 'Unknown'
+	}, {
+		text: 'It is easier to run down a hill than up one.',
+		by: 'Unknown'
+	}, {
+		text: `Unfortunately, most programmers like to play with new toys.  I have many friends who, immediately upon buying a snakebite kit, would be tempted to throw the first person they see to the ground, tie the tourniquet on him, slash him with the knife, and apply suction to the wound.`,
+		by: 'Jon Bentley'
+	}
 ];
 
